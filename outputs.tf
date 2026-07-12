61ae5257aab57614d1f6ae8eedaae882b6cf5e9a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,53 +1,49 @@
-output "spring_cloud_container_deployments" {
-  description = "All spring_cloud_container_deployment resources"
-  value       = azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments
-}
 output "spring_cloud_container_deployments_addon_json" {
-  description = "List of addon_json values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.addon_json]
+  description = "Map of addon_json values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.addon_json }
 }
 output "spring_cloud_container_deployments_application_performance_monitoring_ids" {
-  description = "List of application_performance_monitoring_ids values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.application_performance_monitoring_ids]
+  description = "Map of application_performance_monitoring_ids values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.application_performance_monitoring_ids }
 }
 output "spring_cloud_container_deployments_arguments" {
-  description = "List of arguments values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.arguments]
+  description = "Map of arguments values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.arguments }
 }
 output "spring_cloud_container_deployments_commands" {
-  description = "List of commands values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.commands]
+  description = "Map of commands values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.commands }
 }
 output "spring_cloud_container_deployments_environment_variables" {
-  description = "List of environment_variables values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.environment_variables]
+  description = "Map of environment_variables values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.environment_variables }
 }
 output "spring_cloud_container_deployments_image" {
-  description = "List of image values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.image]
+  description = "Map of image values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.image }
 }
 output "spring_cloud_container_deployments_instance_count" {
-  description = "List of instance_count values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.instance_count]
+  description = "Map of instance_count values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.instance_count }
 }
 output "spring_cloud_container_deployments_language_framework" {
-  description = "List of language_framework values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.language_framework]
+  description = "Map of language_framework values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.language_framework }
 }
 output "spring_cloud_container_deployments_name" {
-  description = "List of name values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.name]
+  description = "Map of name values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.name }
 }
 output "spring_cloud_container_deployments_quota" {
-  description = "List of quota values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.quota]
+  description = "Map of quota values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.quota }
 }
 output "spring_cloud_container_deployments_server" {
-  description = "List of server values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.server]
+  description = "Map of server values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.server }
 }
 output "spring_cloud_container_deployments_spring_cloud_app_id" {
-  description = "List of spring_cloud_app_id values across all spring_cloud_container_deployments"
-  value       = [for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : v.spring_cloud_app_id]
+  description = "Map of spring_cloud_app_id values across all spring_cloud_container_deployments, keyed the same as var.spring_cloud_container_deployments"
+  value       = { for k, v in azurerm_spring_cloud_container_deployment.spring_cloud_container_deployments : k => v.spring_cloud_app_id }
 }
 
